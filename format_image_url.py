d2cde31abc9b8ecfb7b58e5afaae738ad45992eb
eleven-dimension <3096152319@qq.com>
2024-10-15 11:09:20 +0800

    Add logic to handle angle brackets.

--- format_image_url.py
+++ format_image_url.py
@@ -2,24 +2,37 @@ import re
 import os
 
 
+def remove_angle_brackets(s):
+    if s[0] == "<" and s[-1] == ">":
+        return s[1:-1]
+    return s
+
+
 def rename_images(root_path):
     md_file_path = f"{root_path}/main.md"
     with open(md_file_path, "r", encoding="utf-8") as file:
         content = file.read()
 
-    images_names = re.findall(r"!\[.*?\]\((.*?)\)", content)
+    images_names = re.findall(r"!\[.*?\]\((.+)\)", content)
     new_image_name_dict = {}
 
     for image_name in images_names:
         if image_name not in new_image_name_dict:
-            image_extension = re.search(r"\.([a-zA-Z0-9]+)$", image_name).group(1)
+            image_name_without_angle_brackets = remove_angle_brackets(image_name)
+
+            image_extension = re.search(
+                r"\.([a-zA-Z0-9]+)$", image_name_without_angle_brackets
+            ).group(1)
             new_image_name_dict[image_name] = (
                 f"image_{len(new_image_name_dict):02}.{image_extension}"
             )
 
     for old_name, new_name in new_image_name_dict.items():
         content = content.replace(old_name, new_name)
-        os.rename(f"{root_path}/{old_name}", f"{root_path}/{new_name}")
+        old_name_without_angle_brackets = remove_angle_brackets(old_name)
+        os.rename(
+            f"{root_path}/{old_name_without_angle_brackets}", f"{root_path}/{new_name}"
+        )
 
     with open(md_file_path, "w", encoding="utf-8") as file:
         file.write(content)
